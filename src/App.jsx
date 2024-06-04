09869527f28bf3c59a831fc0b67366aa049c0c10
Prapti Gawande <prapting2812@gmail.com>
2024-06-04 19:50:14 +0530

    Projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
-
+import Projects from "./components/Projects";
 
 
 const App = () => {
@@ -12,6 +12,7 @@ const App = () => {
         <Navbar />
         <Home />
         <Skills/>
+        <Projects/>
       </div>
     </>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import projects from "./Data/projects.json";
 const Projects = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="heading my-2">PROJECTS</div>
+      <div className="container projects">
+        <div className="row ">
+          {projects.map((data) => (
+            <>
+              <div key={data.id} className="project my-3 col-md-4 col-sm-6 col-lg-3 mx-3">
+                <div className="card" style={{ width: "18rem" }}>
+                  <div className="img p-3">
+                  <img src={data.imageSrc} className="pimg" alt="..." />
+                  </div>
+                  
+                  <div className="card-body text-center">
+                    <h5 className="card-title">{data.title}</h5>
+                    <p className="card-text">
+                      {data.description}
+                    </p>
+                    <a href={data.demo} className="btn btn-outline-warning mx-3">
+                      Demo
+                    </a>
+                    <a href={data.source} className="btn btn-warning">
+                      Code
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Projects
+export default Projects;
+// justify-content-center align-content-center
