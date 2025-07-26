6571f2e9fabf91eff0133a0c2e29ff256aa288a5
Prapti Gawande <prapting2812@gmail.com>
2025-07-26 20:58:32 +0530

    professionalexperi

--- src/components/ProfessionalEx.jsx
+++ src/components/ProfessionalEx.jsx
@@ -8,8 +8,18 @@ const ProfessionalEx = () => {
         {professionalExperience.map((data) => (
           <div className="professional-ex-card" key={data.id}>
             <div className="professional-ex-header">
-              <h3>{data.role}</h3>
-              <span className="professional-ex-date">{data.date} - {data.endDate}</span>
+              {data.logo && (
+                <img
+                  src={`/assets/${data.logo}`}
+                  alt="Company Logo"
+                  className="professional-ex-logo"
+                  style={{ height: "60px", marginRight: "1rem" }}
+                />
+              )}
+              <div>
+                <h3>{data.role}</h3>
+                <span className="professional-ex-date">{data.date} - {data.endDate}</span>
+              </div>
             </div>
             <ul className="professional-ex-list">
               {data.experiences.map((exp, idx) => (
